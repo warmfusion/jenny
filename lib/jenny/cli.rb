62d0f83367332add4eba0fdbeed9851c26d0c769
Toby Jackson <admin@warmfusion.co.uk>
2015-02-07 14:40:09 +0000

    Update to a 0.1.0 release and support --version, -v options to get the current version - http://stackoverflow.com/a/27804972/905507

--- lib/jenny/cli.rb
+++ lib/jenny/cli.rb
@@ -3,6 +3,13 @@ require 'thor'
 module Jenny
   class CLI < Thor
 
+    map %w[--version -v] => :__print_version
+
+    desc "--version, -v", "print the version"
+    def __print_version
+      puts Jenny::VERSION
+    end
+
     desc 'vagrant HOSTNAME', 'Create a new Vagrant configuration with HOSTNAME given'
     method_option "fqdn", :type => :string, :aliases => '-f', :default => 'local', :desc => 'The FQDN suffix for your hostname' 
     def vagrant(hostname)
